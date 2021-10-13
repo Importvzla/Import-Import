667b2b07fb10e04ccdcf576d21c8d9858b485ee5
Fabio Tamburini <fatm400@yandex.com>
2021-10-13 15:10:49 -0400

    agregar modulo bi_purchase_tripple_approval

---
+++ bi_purchase_tripple_approval/__manifest__.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+# Part of Browseinfo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Purchase Tripple Approval validation Process',
+    'version': '14.0.0.0',
+    'category': 'Purchases',
+    'sequence': 15,
+    'summary': 'purchase triple approve purchase order tripple Validation Approval purchase three approval process purchase triple validation purchase order double approval purchase approval workflow purchase order triple approval purchase manager approval 3step purchase',
+    'description': """This app helps you to tripple approval process for Purchase Order while confirm Purchase order.
+    Purchase Double Validation,
+    purchases Double approval workflow
+    purchases Approval workflow
+    purchases validation workflow
+    purchases double validation
+    purchase approval workflow
+    purchase two step approval
+    purchase two step validation
+    double validation on purchases
+    approval workflow on purchases
+    Validation process on purchase
+    Double validation on purchases
+    Confirm purchase approval
+	three step purchase approval
+	three step purchase validation
+	
+	
+
+    Purchase Tripple Validation,
+    purchases Tripple approval workflow
+    purchases Tripple workflow
+    purchases Tripple validation
+    purchase Tripple approval workflow
+    purchase three step approval
+    purchase three step validation
+    Tripple validation on purchases
+    tripple validation on purchases
+    three Confirm purchase approval   
+
+    RFQ Double Validation,
+    RFQ Double approval workflow
+    RFQ Approval workflow
+    RFQ validation workflow
+    RFQ double validation
+    RFQ approval workflow
+    RFQ two step approval
+    RFQ two step validation
+    double validation on RFQ
+    approval workflow on RFQ
+    Validation process on RFQ
+    Double validation on RFQ
+    Confirm RFQ approval
+
+    RFQ Tripple Validation,
+    RFQ Tripple approval workflow
+    RFQ Approval workflow
+    RFQ validation workflow
+    RFQ Tripple validation
+    RFQ approval workflow
+    RFQ three step approval
+    RFQ three step validation
+    Tripple validation on RFQ
+    approval workflow on RFQ
+    Validation process on RFQ
+    Tripple validation on RFQ
+    Confirm RFQ approval
+        User based condition on Purchase order double validation, Dynamic condition on Purchase Order double validation Process, Dynamic condition on Purchase Order double Approval Process, User based condtion for purchase approval, Purchase order approval process, purchase order limit verification, purchase order user based limit, purchase order user based approval condition.
+ 
+ Odoo Sales Double Validation sale double approve validation sale double validate sales double validate
+    Odoo Sales Double approval workflow Sales Approval workflow Sales validation workflow Sale double validation
+    Odoo sale approval workflow sale two step approval sale two step validation double validation on sales
+    Odoo approval workflow on sales Validation process on sales Double validation on sales Confirm sales approval
+
+    Odoo Quotation Double Validation Quotation Double approval workflow Quotation Approval workflow Quotation validation workflow
+    Odoo Quotation double validation Quotation approval workflow Quotation two step approval Quotation two step validation
+    Odoo double validation on quotation approval workflow on quotation Validation process on quotation Double validation on quotation Confirm Quotation approval
+    
+    Odoo Sales order Double Validation Sale order Double approval workflow Sales order Double approval workflow
+    Odoo Sale order Double approval workflow Odoo Sales order Approval workflow
+    Odoo Sales order validation workflow Sales order double validation Sales double approval Sales two time approval 
+    Odoo sale approval Process Sale order approval workflow Sale double step approval sale aprroval process for sale
+    Odoo Sales Order two step approval Sales Order two step validation sale double validation on sales
+    approval workflow on sales Validation process on sales Double validation on sales Confirm sale approval
+
+
+    Odoo Quotation Double Validation Quotation Double approval workflow Quotation Approval workflow
+    Odoo Quotation validation workflow Quotation double validation Quotation approval workflow
+    Odoo Quotation two step approval Quotation two step validation
+    odoo double validation on Quotation approval workflow on Quotation Validation process on Quotation
+    Odoo Double validation on Quotation Confirm customer order approval sale double approve
+    Odoo Sales Tripple Approval Process Sales three step approval process
+    Odoo Sale tripple validation processs tripple validation on Sales tripple approval on Quotation
+    Odoo tripple approval on Sales tripple validation on Sales order tripple validation on Quotation order
+    Odoo tripple approval on sales order three step approval on sales order three step approval on Quotation
+    Odoo three step approval on Sales order three step approval on Sale order three step approval on customer order
+    Odoo three step validation on Sale three step validation on sales orders three step validation on quotation three step validation on sales order 
+    Odoo three step validation on Quotation Odoo Quotation three step validation on Sales order
+    Odoo Sale three step approval Sale multi-step validation Quotation multi step validation
+    Odoo Quotation Tripple Validation Quotation Tripple approval workflow Quotation Tripple workflow Quotation Tripple validation
+    odoo Quotation Tripple approval workflow Quotation three step approval Quotation three step validation
+    Odoo Tripple validation on Quotation tripple validation on Sale
+    Odoo three Confirm customer sales approval   
+
+
+    Odoo Sale order Tripple Validation Sale order Tripple approval workflow Sale order  Approval workflow
+    Odoo Sale order validation workflow Sale order Tripple validation Sale order approval workflow Sale order three step approval
+    Odoo Sale order three step validation Tripple validation on Sale order approval workflow on Sale order
+    Odoo Validation process on Sale order Tripple validation on Sale order Confirm Sale order approval
+
+    Odoo purchase Double Validation purchase double approve validation purchase double validate purchases double validate
+    Odoo purchase Double approval workflow purchase Approval workflow purchase validation workflow purchase double validation
+    Odoo purchase approval workflow purchase two step approval purchase two step validation double validation on purchases
+    Odoo approval workflow on purchase Validation process on purchase Double validation on purchase Confirm purchases approval
+
+    Odoo RFQ Double Validation RFQ Double approval workflow RFQ Approval workflow RFQ validation workflow
+    Odoo RFQ double validation Quotation approval workflow Quotation two step approval RFQ two step validation
+    Odoo double validation on RFQ approval workflow on quotation Validation process on RFQ Double validation on RFQ Confirm RFQ approval
+    
+    Odoo purchase order Double Validation purchase order Double approval workflow purchase order Double approval workflow
+    Odoo purchase order Double approval workflow Odoo purchase order Approval workflow
+    Odoo purchase order validation workflow purchase order double validation purchase double approval purchase two time approval 
+    Odoo purchase approval Process purchase order approval workflow purchase double step approval purchase Approval process for purchase
+    Odoo purchase Order two step approval purchase Order two step validation purchase double validation on purchases
+    approval workflow on purchase Validation process on purchase Double validation on purchase Confirm purchase approval
+
+
+    Odoo purchase Double Validation purchase Double approval workflow purchase Approval workflow
+    Odoo purchase validation workflow purchase double validation purchase approval workflow
+    Odoo purchase two step approval purchase two step validation
+    odoo double validation on purchase approval workflow on purchase Validation process on purchase
+    Odoo Double validation on purchase Confirm customer order approval purchase double approve purchase
+    Odoo purchase Tripple Approval Process purchase three step approval process
+    Odoo purchase tripple validation processs tripple validation on purchase tripple approval on purchase
+    Odoo tripple approval on purchase tripple validation on purchase order tripple validation on purchase order
+    Odoo tripple approval on purchase order three step approval on purchase order three step approval on purchase
+    Odoo three step approval on purchase order three step approval on purchase order three step approval on customer order
+    Odoo three step validation on purchase three step validation on sales orders three step validation on quotation three step validation on sales order 
+    Odoo three step validation on purchase Odoo purchase three step validation on purchase order
+    Odoo purchase three step approval purchase multi-step validation purchase multi step validation
+    Odoo purchase Tripple Validation purchase Tripple approval workflow purchase Tripple workflow purchase Tripple validation
+    odoo purchase Tripple approval workflow purchase three step approval purchase three step validation
+    Odoo Tripple validation on purchase tripple validation on purchase
+    Odoo three Confirm customer purchase approval   
+
+
+    Odoo purchase order Tripple Validation purchase order Tripple approval workflow purchase order  Approval workflow
+    Odoo purchase order validation workflow purchase order Tripple validation purchase order approval workflow purchase order three step approval
+    Odoo purchase order three step validation Tripple validation on purchase order approval workflow on purchase order
+    Odoo Validation process on purchase order Tripple validation on purchase order Confirm purchase order approval
+
+    odoo purchase Tripple Validation purchase Tripple approval workflow purchase Approval workflow purchase validation workflow
+    odoo purchase Tripple validation purchase approval workflow purchase three step approval purchase three step validation
+
+
+    User based condition on purchase order double validation Dynamic condition on purchase Order double validation Process Dynamic condition on purchase Order double Approval Process User based condtion for purchase approval purchase order approval process, 
+    purchase order limit verification purchase order user based limit purchase order user based approval condition.
+      
+
+    """,
+    'website': 'https://www.browseinfo.in',
+    'price': 49,
+    'currency': "EUR",
+    'author': 'BrowseInfo',
+    'depends': ['base','purchase','stock'],
+    'data': [
+            'security/purchase_groups.xml',
+            'views/purchase_config_setting_view.xml',
+            'views/purchase_order_view.xml'],
+    'demo': [],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'live_test_url' : 'https://youtu.be/4wPym9SXnyQ',
+    "images":['static/description/Banner.png'],
+}
