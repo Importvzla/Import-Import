45a3ad18475c4802be5322a234d42f559bd3bee0
Fabio Tamburini <fatm400@gmail.com>
2021-11-03 10:15:25 -0400

    comprobante de retencion de iva, segunda version

--- import_customization/reports/import_import_IVA_voucher.py
+++ import_customization/reports/import_import_IVA_voucher.py
@@ -11,49 +11,40 @@ class ImportImportIslrVoucher(models.AbstractModel):
         print('funcion para obtener datos del cliente para el reporte')
         docs = self.env['account.move'].browse(docids[0])
 
-        # data_islr = []
-        # tax_withheld = 0.0
-        #
-        # islr_voucher_line = {
-        #     'payment_date': docs.invoice_date,
-        #     'document_number': docs.ref,
-        #     'control_No': docs.x_studio_nro_control,
-        #     'amount_paid': 0.0,
-        #     'amount_document': 0.0,
-        #     'amount_obt': 0.0,
-        #     'retention_percentage': 0.0,
-        #     'ret_cod': '',
-        #     'description': '',
-        #     'tax_withheld': 0.0,
-        # }
-        #
-        #
-        # for ili in docs.invoice_line_ids:
-        #     for ti in ili.tax_ids:
-        #         if ti.x_tipoimpuesto == 'ISLR':
-        #             line_id = docs.line_ids.search([('name', '=', ti.name), ('move_id', '=', docs.id)])
-        #             print(line_id)
-        #             islr_voucher_line.update(
-        #                 amount_document=ili.price_subtotal,
-        #                 amount_obt=ili.price_subtotal,
-        #                 retention_percentage=ti.amount,
-        #                 ret_cod=ti.name[:6],
-        #                 description=ti.name[7:],
-        #                 tax_withheld=line_id.debit,
-        #             )
-        #             data_islr.append(islr_voucher_line)
-        #             tax_withheld += line_id.debit
-        #
-        # if not data_islr:
-        #     data_islr.append(islr_voucher_line)
-        #
-        # print(data_islr)
+        fiscal_period = str(docs.invoice_date.month) + "-" + str(docs.invoice_date.year)
+        print(fiscal_period)
+
+        exempt_sum = 0.0
+        tax_base = 0.0
+        retention_percentage = ''
+        tax_iva = 0.0
+        iva_withheld = 0.0
+
+        for ili in docs.invoice_line_ids:
+            for ti in ili.tax_ids:
+                if ti.x_tipoimpuesto == 'IVA':
+                    tax_base += ili.price_subtotal
+                    line_iva_id = docs.line_ids.search([('name', '=', ti.name), ('move_id', '=', docs.id)])
+                    tax_iva = line_iva_id.debit
+                    retention_percentage = line_iva_id.name
+                if ti.x_tipoimpuesto == 'EXENTO':
+                    exempt_sum += ili.price_subtotal
+                if  ti.x_tipoimpuesto == 'RIVA':
+                    line_riva_id = docs.line_ids.search([('name', '=', ti.name), ('move_id', '=', docs.id)])
+                    iva_withheld = line_riva_id.credit
+
 
         docargs = {
             'doc_ids': docids,
             'doc_model': 'account.move',
             'data': data,
             'docs': docs,
+            'fiscal_period': fiscal_period,
+            'exempt_sum': exempt_sum,
+            'tax_base': tax_base,
+            'retention_percentage': retention_percentage,
+            'tax_iva': tax_iva,
+            'iva_withheld': iva_withheld,
             # 'data_islr': data_islr,
             # 'tax_withheld': tax_withheld,
         }
