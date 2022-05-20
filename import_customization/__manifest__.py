a106eba881334998ba6aba55181b4b8e9ceda34d
Fabio Tamburini <fatm400@gmail.com>
2022-05-20 14:08:04 -0400

    modificar numero de orden de venta

--- import_customization/__manifest__.py
+++ import_customization/__manifest__.py
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Fabio Tamburini",
+    'website': "",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -26,6 +26,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'data/invoice_paper_format.xml',
+        'data/res_country.xml',
         'views/views.xml',
         'views/import_import_report.xml',
         'views/res_partner_views.xml',
