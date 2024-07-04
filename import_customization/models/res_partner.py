b9ac40ff522366bcbbb3d1cf17c521ba4c139620
Fabio Tamburini <fatm400@gmail.com>
2024-07-04 15:11:22 -0400

    feat(import_customization): agregar nueva factura tipo 4 para USD

--- import_customization/models/res_partner.py
+++ import_customization/models/res_partner.py
@@ -4,15 +4,24 @@ from odoo import models, fields, api, _
 
 
 class CustomResPartner(models.Model):
-    _inherit = 'res.partner'
-    _description = 'modificar modelo de res_partner'
+    _inherit = "res.partner"
+    _description = "modificar modelo de res_partner"
 
-    type_invoice_usd = fields.Selection([('type_1', 'Tipo 1'), ('type_2', 'Tipo 2'), ('type_3', 'Tipo 3')],
-                                    string='Tipo de Factura USD', default='type_1'
-                                    )
+    type_invoice_usd = fields.Selection(
+        [
+            ("type_1", "Tipo 1"),
+            ("type_2", "Tipo 2"),
+            ("type_3", "Tipo 3"),
+            ("type_4", "Tipo 4"),
+        ],
+        string="Tipo de Factura USD",
+        default="type_1",
+    )
 
-    type_invoice_ves = fields.Selection([('type_1', 'Tipo 1'), ('type_2', 'Tipo 2')],
-                                        string='Tipo de Factura Bs', default='type_1'
-                                        )
+    type_invoice_ves = fields.Selection(
+        [("type_1", "Tipo 1"), ("type_2", "Tipo 2")],
+        string="Tipo de Factura Bs",
+        default="type_1",
+    )
 
     # type_invoice = fields.Char('xxxx')
